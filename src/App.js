390616267549ee901a42678c91480a5183ca911e
glaita <glaita@users.noreply.github.com>
2020-10-29 14:40:16 -0300

    - updated all dependencies (#7)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,7 +19,7 @@ function App() {
         </a>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
